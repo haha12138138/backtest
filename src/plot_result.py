86695be0f4135c066837e681186eb0492c9ed8c9
haha12138138 <tige.rzz@163.com>
2024-11-02 21:05:05 +0800

    [FEAT] support multiple result comparision

--- src/plot_result.py
+++ src/plot_result.py
@@ -1,15 +1,23 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+import os
 
-df = pd.read_csv(r"../TEST.hold_all.csv")
-df1 = pd.read_csv(r"../result/DGRW.GPOE.hold_all.csv")
-df.set_index("Date",inplace=True)
-df1.set_index("Date",inplace=True)
-df = df.astype(float)
-df1 = df1.astype(float)
-df = df / df.iloc[0]
-df1 = df1 / df1.iloc[0]
-df1.rename(columns={"Portfolio":"DF1"},inplace=True)
-df.rename(columns={"Portfolio":"DF"},inplace=True)
-pd.concat([df1['DF1'],df['DF']],axis=1).plot()
+import matplotlib.pyplot as plt
+import pandas as pd
+
+results = [
+    r"../results/Fundamental_DGRW/GPOE_TTM.PEG.csv",
+    r"../results/Fundamental_DGRW/GPOE_TTM.csv",
+    r"../results/Fundamental_DGRW/GPOE_LYR.csv",
+    r"../results/Fundamental_DGRW/GPOE_TTM.PB_PE_PEG.csv",
+    r"../results/Fundamental_DGRW/GPOE_TTM.PB_PEG.csv"
+]
+dfs = []
+for r in results:
+    fname = os.path.splitext(os.path.basename(r))[0]
+    df = pd.read_csv(r)
+    df.set_index("Date", inplace=True)
+    df.drop(["Benchmark"], axis="columns", inplace=True)
+    df.rename(columns={"Portfolio": fname}, inplace=True)
+    dfs.append(df)
+
+pd.concat(dfs, axis=1).plot()
 plt.show()
