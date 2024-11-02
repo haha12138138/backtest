2e9c0aac9bcb8e2bdd4b2884c77a35bf528dd94e
haha12138138 <tige.rzz@163.com>
2024-11-02 21:06:11 +0800

    [MISC] update some examples

--- user_strategies/SIMPLE_FUNDAMANTAL_STRATEGY.py
+++ user_strategies/SIMPLE_FUNDAMANTAL_STRATEGY.py
@@ -1,16 +1,19 @@
 import numpy as np
 import pandas as pd
+
 from Backtester import Backtester
+from FMP.fmp import Income_statement_entry, Balance_statement_entry, Datapoint_period, Financial_ratio_entry
 from Strategy import Strategy, rebalance, Strategy_dependence
 from Universe import Stock_Universe
 
-from FMP.fmp import Income_statement_entry, Balance_statement_entry, Datapoint_period
 
-
-class Simple_Fundamental_Stratery(Strategy):
+class GPOE_Value(Strategy):
     inherent_dependencies = Strategy.inherent_dependencies + [
-        Strategy_dependence(Income_statement_entry.gross_profit, Datapoint_period.annual, 1),
-        Strategy_dependence(Balance_statement_entry.total_equity, Datapoint_period.annual, 1)
+        Strategy_dependence(Income_statement_entry.gross_profit, Datapoint_period.quarter, 4),
+        Strategy_dependence(Balance_statement_entry.total_equity, Datapoint_period.quarter, 4),
+        Strategy_dependence(Financial_ratio_entry.PB, Datapoint_period.quarter, num_of_data_needed=1),
+        Strategy_dependence(Financial_ratio_entry.PE, Datapoint_period.quarter, num_of_data_needed=1),
+        Strategy_dependence(Financial_ratio_entry.PEG, Datapoint_period.quarter, num_of_data_needed=1)
     ]
 
     def __init__(self):
@@ -21,6 +24,108 @@ class Simple_Fundamental_Stratery(Strategy):
         def z_score(df: pd.Series):
             return (df - df.mean()) / df.std()
 
+        def msci_mom_normalize(series_A):
+            # Step 1: Create Series B based on the rules given for Series A
+            series_B = series_A  # series_A.clip(lower=-3, upper=3)
+
+            # Step 2: Apply the transformation rules for Series B
+            def update_value(Z):
+                if Z > 0:
+                    return 1 + Z
+                elif Z < 0:
+                    return 1 / (1 - Z)
+                else:
+                    return Z  # If Z == 0, it remains 0
+
+            series_B_transformed = series_B.apply(update_value)
+
+            return series_B_transformed
+        def equal_weights(N, M):
+            weights = np.array([1 for i in range(N)])
+            # Normalize the weights so they sum to 1
+            normalized_weights = weights / np.sum(weights)
+            return normalized_weights
+
+        # ROE = self.data_handler.get_fundamentals(metric=Financial_ratio_entry.ROE,prev=1)
+        GP = self.data_handler.get_fundamentals(metric=Income_statement_entry.gross_profit, prev=4)
+        equity = self.data_handler.get_fundamentals(metric=Balance_statement_entry.total_equity, prev=4)
+        PB = self.data_handler.get_fundamentals(metric=Financial_ratio_entry.PB, prev=1)
+        PE = self.data_handler.get_fundamentals(metric=Financial_ratio_entry.PE, prev=1)
+        PEG = self.data_handler.get_fundamentals(metric=Financial_ratio_entry.PEG, prev=1)
+        for e in (GP, equity, PB, PE, PEG):
+            if e is None:
+                return None
+        GP.dropna(axis=1, how="any", inplace=True)
+        equity.dropna(axis=1, how="any", inplace=True)
+        PB.dropna(axis=1, how="any", inplace=True)
+        PE.dropna(axis=1, how="any", inplace=True)
+        PEG.dropna(axis=1, how="any", inplace=True)
+        GP_TTM = GP.sum().to_frame().T
+        equity_TTM = equity.mean().to_frame().T
+
+        uni = self.data_handler.get_dynamic_universe()
+        uni = list(set(uni)
+                   .intersection(set(equity_TTM.iloc[0].index))
+                   .intersection(set(GP_TTM.iloc[0].index))
+                   .intersection(set((PB.iloc[0] > 1).index))
+                   # .intersection(set((PE.iloc[0]).index))
+                   .intersection(set((PEG.iloc[0]).index))
+                   )
+
+        if len(uni) == 0:
+            return None
+        GPOE_z = z_score((GP_TTM / equity_TTM)[uni].iloc[0]).clip(lower=-3, upper=3)
+
+        ranked_securities = list(GPOE_z.rank(ascending=True).sort_values(ascending=False).index)
+        N = int(len(ranked_securities) / 5 + 1)
+        selected = ranked_securities[0:N]
+        # Value_z = msci_mom_normalize(z_score(1/z_score(PB[selected.index].iloc[0]) + 1/z_score(PE[selected.index].iloc[0])))
+        value_z = msci_mom_normalize(z_score(
+            -1 * z_score(PEG[selected].iloc[0]) +
+            -1 * z_score(PB[selected].iloc[0])
+            # -1 * z_score(PE[selected].iloc[0])
+        ))
+
+        signals = {}
+        for s, w1 in (value_z / value_z.sum()).items():
+            signals[s] = w1
+
+        return signals
+
+
+class GPOE_LYR(Strategy):
+    inherent_dependencies = Strategy.inherent_dependencies + [
+        Strategy_dependence(Income_statement_entry.gross_profit, Datapoint_period.annual, 1),
+        Strategy_dependence(Balance_statement_entry.total_equity, Datapoint_period.annual, 1),
+        # Strategy_dependence(Financial_ratio_entry.PB,Datapoint_period.quarter,num_of_data_needed=1),
+        # Strategy_dependence(Financial_ratio_entry.PEG,Datapoint_period.quarter,num_of_data_needed=1)
+    ]
+
+    def __init__(self):
+        super().__init__()
+
+    @rebalance(60)
+    def generate_signals(self):
+        def z_score(df: pd.Series):
+            return (df - df.mean()) / df.std()
+
+        def msci_mom_normalize(series_A):
+            # Step 1: Create Series B based on the rules given for Series A
+            series_B = series_A  # series_A.clip(lower=-3, upper=3)
+
+            # Step 2: Apply the transformation rules for Series B
+            def update_value(Z):
+                if Z > 0:
+                    return 1 + Z
+                elif Z < 0:
+                    return 1 / (1 - Z)
+                else:
+                    return Z  # If Z == 0, it remains 0
+
+            series_B_transformed = series_B.apply(update_value)
+
+            return series_B_transformed
+
         def equal_weights(N, M):
             weights = np.array([1 for i in range(N)])
             # Normalize the weights so they sum to 1
@@ -30,96 +135,46 @@ class Simple_Fundamental_Stratery(Strategy):
         # ROE = self.data_handler.get_fundamentals(metric=Financial_ratio_entry.ROE,prev=1)
         GP = self.data_handler.get_fundamentals(metric=Income_statement_entry.gross_profit, prev=1)
         equity = self.data_handler.get_fundamentals(metric=Balance_statement_entry.total_equity, prev=1)
+        # PB = self.data_handler.get_fundamentals(metric=Financial_ratio_entry.PB, prev=1)
+        # PEG = self.data_handler.get_fundamentals(metric=Financial_ratio_entry.PEG,prev=1)
+        for e in (GP, equity):
+            if e is None:
+                return None
+        GP.dropna(axis=1, how="any", inplace=True)
+        equity.dropna(axis=1, how="any", inplace=True)
+        # PB.dropna(axis=1, how="all", inplace=True)
+        # PEG.dropna(axis=1, how="all", inplace=True)
+        GP_TTM = GP
+        equity_TTM = equity
 
-        if GP is None or equity is None:
-            return None
-        GP.dropna(axis=1, how="all", inplace=True)
-        equity.dropna(axis=1, how="all", inplace=True)
-        # ROE.dropna(axis=0,how="all",inplace=True)
         uni = self.data_handler.get_dynamic_universe()
-        uni = list(set(GP.iloc[0].index).intersection(set(equity.iloc[0].index)).intersection(set(uni)))
-        GPOE_z = z_score((GP / equity)[uni].iloc[0]).clip(lower=-3, upper=3)
+        uni = list(set(uni)
+                   .intersection(set(equity_TTM.iloc[0].index))
+                   .intersection(set(GP_TTM.iloc[0].index))
+                   # .intersection(set((PB.iloc[0] >1).index))
+                   # .intersection(set((PEG.iloc[0] >0).index))
+                   )
+
+        if len(uni) == 0:
+            return None
+        GPOE_z = z_score((GP_TTM / equity_TTM)[uni].iloc[0]).clip(lower=-3, upper=3)
+
         ranked_securities = list(GPOE_z.rank(ascending=True).sort_values(ascending=False).index)
         N = int(len(ranked_securities) / 5 + 1)
-        w = equal_weights(N, N)
+        selected = ranked_securities[0:N]
+        # Value_z = msci_mom_normalize(z_score(1/z_score(PB[selected.index].iloc[0]) + 1/z_score(PE[selected.index].iloc[0])))
+        # value_z = msci_mom_normalize(z_score(1/(PEG[selected.index].iloc[0])))
         signals = {}
-        for s, w1 in zip(ranked_securities[0:N], w):
+        for s, w1 in zip(ranked_securities[0:N], equal_weights(N, N)):
             signals[s] = w1
 
         return signals
-
-
-# class PM_ratio(Strategy):
-#     inherent_dependencies = Strategy.inherent_dependencies
-#     def __init__(self):
-#         super().__init__()
-#
-#     @rebalance(60)
-#     def generate_signals(self):
-#         def z_score(df: pd.Series):
-#             return (df - df.mean()) / df.std()
-#
-#         def equal_weights(N, M):
-#             weights = np.array([1 for i in range(N)])
-#             # Normalize the weights so they sum to 1
-#             normalized_weights = weights / np.sum(weights)
-#             return normalized_weights
-#
-#         def calc_score(series_A):
-#             # Step 1: Create Series B based on the rules given for Series A
-#             series_B = series_A.clip(lower=-3, upper=3)
-#
-#             # Step 2: Apply the transformation rules for Series B
-#             def update_value(Z):
-#                 if Z > 0:
-#                     return 1 + Z
-#                 elif Z < 0:
-#                     return 1 / (1 - Z)
-#                 else:
-#                     return Z  # If Z == 0, it remains 0
-#
-#             series_B_transformed = series_B.apply(update_value)
-#
-#             return series_B_transformed
-#
-#         def z_score(df):
-#             return (df - df.mean()) / df.std()
-#
-#         LEV = self.data_handler.get_fundamentals(metric=Financial_ratio_entry.equity_multiplier, prev=1)
-#         D = self.data_handler.get_fundamentals(metric=Balance_statement_entry.total_debt, prev=1)
-#         I = self.data_handler.get_fundamentals(metric=Income_statement_entry.interest_expense, prev=1)
-#         GP = self.data_handler.get_fundamentals(metric=Income_statement_entry.gross_profit, prev=1)
-#         Asset = self.data_handler.get_fundamentals(metric=Balance_statement_entry.total_assets, prev=1)
-#         if LEV is None or D is None or I is None:
-#             return None
-#         I.dropna(axis=1, how="all", inplace=True)
-#         D.dropna(axis=1, how="all", inplace=True)
-#         LEV.dropna(axis=1, how="all", inplace=True)
-#         GP.dropna(axis=1, how="all", inplace=True)
-#         Asset.dropna(axis=1, how="all", inplace=True)
-#         uni = self.data_handler.get_dynamic_universe()
-#         uni = list(set((LEV).iloc[0].index).intersection(set((I).iloc[0].index)).intersection(
-#             set((D).iloc[0].index)).intersection(set(uni)))
-#         LEV = LEV[uni].iloc[0]
-#         Interest_rate_inv = (D / I)[uni].iloc[0]
-#         GPOA = GP[uni].iloc[0] / Asset[uni].iloc[0]
-#         comp_factor = calc_score(GPOA.rank(ascending=True) + (LEV * Interest_rate_inv).rank(ascending=True))
-#         ranked_securities = list(comp_factor.rank(ascending=True).sort_values(ascending=False).index)
-#         N = int(len(ranked_securities) / 5 + 1)
-#         # w = equal_weights(N,N)
-#         w = comp_factor[ranked_securities[0:N]] / comp_factor[ranked_securities[0:N]].sum()
-#         signals = {}
-#         for s, w1 in zip(ranked_securities[0:N], w):
-#             signals[s] = w1
-#
-#         return signals
-
 
 if __name__ == "__main__":
     # 使用示例
     # universe = ['XLK', 'XLV', 'XLE']
 
-    start_date = '2013-11-01'
+    start_date = '2019-01-01'
     end_date = '2024-09-13'
     initial_cash = 10000
     # backtester = Backtester(universe
@@ -128,12 +183,13 @@ if __name__ == "__main__":
     #                         , strategy= MOM_STRATEGY
     #                         , benchmark="QQQ")
     uni = Stock_Universe()
-    uni.add_holdings_to_group("DGRW", "index", topN=2)
+    uni.add_holdings_to_group("DGRW", "index", topN=30)
     # uni.add_holdings_to_group("XMHQ", "index", topN= 10)
     uni.add_benchmark("SPY")
     backtester = Backtester(uni
                             , start_date, end_date, initial_cash
-                            , strategy=Simple_Fundamental_Stratery)
+                            , strategy=GPOE_Value)
     backtester.run_backtest()
-    backtester.portfolio.position_history.set_index("Date").to_csv("TEST.holdings.csv")
-    backtester.portfolio.history.set_index("Date").to_csv("TEST.hold_all.csv")
+    backtester.portfolio.position_history.set_index("Date").to_csv(
+        r"../results/Fundamental_DGRW/GPOE_TTM.PB_PEG.holdings.csv")
+    backtester.portfolio.history.set_index("Date").to_csv("../results/Fundamental_DGRW/GPOE_TTM.PB_PEG.csv")
