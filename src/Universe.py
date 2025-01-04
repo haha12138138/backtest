b2bd3382a71857b9a93682fe1369698dd9eb0ca7
tiger <tige.rzz@163.com>
2025-01-04 16:24:10 +0800

    [DOC] some doc added

--- src/Universe.py
+++ src/Universe.py
@@ -2,12 +2,28 @@ from FMP.fmp import *
 
 
 class Stock_Universe:
+    """
+       A class to manage and organize a universe of stocks based on different groups and requirements.
+       The class allows adding stocks or ETFs to specific groups, loading data for these groups, and retrieving the universe of stocks.
+    """
     def __init__(self):
+        """
+           A class to manage and organize a universe of stocks based on different groups and requirements.
+           The class allows adding stocks or ETFs to specific groups, loading data for these groups, and retrieving the universe of stocks.
+        """
         self.__requirement = {'default_group': []}
         self.__groups = {"default_group": None}
         self.__total_universe = set()
 
     def add_holdings_to_group(self, ticker, type, topN=1, group_name='default_group'):
+        """
+                Adds a stock or ETF to a specific group with a given type and topN value.
+
+                :param ticker: The ticker symbol of the stock or ETF.
+                :param type: The type of the asset (e.g., "stock", "index").
+                :param topN: The number of top holdings to consider (default is 1).
+                :param group_name: The name of the group to which the asset is added (default is 'default_group').
+        """
         if group_name in self.__requirement.keys():
             self.__requirement[group_name].append((ticker, type, topN))
         else:
@@ -15,9 +31,21 @@ class Stock_Universe:
             self.__requirement[group_name].append((ticker, type, topN))
 
     def add_benchmark(self, ticker):
+        """
+                Adds a benchmark stock to the 'benchmark' group.
+
+                :param ticker: The ticker symbol of the benchmark stock.
+        """
         self.add_holdings_to_group(ticker, type="stock", group_name="benchmark")
 
     def load_data(self, start_date, end_date):
+        """
+                Loads data for all groups based on the specified start and end dates.
+
+                :param start_date: The start date for data retrieval in "YYYY-MM-DD" format.
+                :param end_date: The end date for data retrieval in "YYYY-MM-DD" format.
+                :return: 0 indicating successful data loading.
+        """
         for name, assets_info in self.__requirement.items():
             dfs = []
             individual_stocks = []
@@ -44,10 +72,10 @@ class Stock_Universe:
 
     def get_universe(self, name):
         """
-        get the holding data of a group.
-        should only be called by the data_handler
-        :param name: name of the group
-        :return: Error or the data:pd.DataFrame
+        Retrieves the holdings data for a specific group.
+
+        :param name: The name of the group.
+        :return: The holdings data as a pandas DataFrame, or None if the group does not exist.
         """
         if name not in self.__groups.keys():
             # raise ValueError(f"{name} is not among the group names: {list(self.__groups.keys())}")
@@ -56,4 +84,9 @@ class Stock_Universe:
             return self.__groups[name]
 
     def get_total_universe(self):
+        """
+                Retrieves the total universe of stocks across all groups.
+
+                :return: A set containing all the stocks in the universe.
+        """
         return self.__total_universe
